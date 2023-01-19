1f9042a22d17f5ce82fa23cfec87559f4d00011c
Rahul Nag <rahulnag625@gmail.com>
2023-01-19 22:05:26 +0530

    removed react-snap

--- src/index.js
+++ src/index.js
@@ -1,34 +1,35 @@
 import React from "react";
 import { hydrate, render } from "react-dom";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
 // below changes are made based on this: https://web.dev/prerender-with-react-snap/
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    rootElement
-  );
-} else {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    rootElement
-  );
-}
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
+// const rootElement = document.getElementById("root");
+// if (rootElement.hasChildNodes()) {
+//   hydrate(
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>,
+//     rootElement
+//   );
+// } else {
+//   render(
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>,
+//     rootElement
+//   );
+// }
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 
-//   document.getElementById("root")
-// );
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
